d172deae6ebe3dd12541a19176e18ce9e6af8fcf
Tomasz Pituch <tomaszpituch@gmail.com>
2023-09-05 17:29:32 +0200

    rebase

--- Person.java
+++ Person.java
@@ -1,13 +1,19 @@
+import java.math.BigDecimal;
+
 public class Person {
 
     private String pesel;
     private String name;
+    private BigDecimal salary;
+
 
     private static Integer HEIGHT = 100;
+
 
 
     private String whatIsYourHobby(){
         return "pizza";
     }
+
 
 }
