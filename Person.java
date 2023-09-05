e49bf9eb4451016a48da528760c7786cfae25087
Tomasz Pituch <tomaszpituch@gmail.com>
2023-09-05 17:29:10 +0200

    feat: Rebase

--- Person.java
+++ Person.java
@@ -1,9 +1,13 @@
 public class Person {
+
     private String pesel;
+    private String name;
 
     private static Integer HEIGHT = 100;
+
 
     private String whatIsYourHobby(){
         return "pizza";
     }
+
 }
