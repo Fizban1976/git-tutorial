3f0ca0c2434d66096d6f546abfc10dc4130c0eb2
Tomasz Pituch <tomaszpituch@gmail.com>
2023-09-05 18:01:01 +0200

    feat: [TASK-00005] Full reset and gitignore update

--- Person.java
+++ Person.java
@@ -1,21 +1,2 @@
-import java.math.BigDecimal;
-
 public class Person {
-
-    private String pesel;
-    private String name;
-    private BigDecimal salary;
-
-
-    private static Integer HEIGHT = 100;
-
-
-    private String whatIsYourHobby(){
-        return "pizza";
-    }
-
-    public void method(){
-        //some content
-    }
-
 }
